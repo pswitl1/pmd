c30187c089ed787c007fc951589de6eef3b186cc
Phil Switlick <pswitl1@students.towson.edu>
2019-03-12 14:34:52 -0400

    improvements to UncontrolledFormatStringRule

--- cwe_tests/UncontrolledFormatString.java
+++ cwe_tests/UncontrolledFormatString.java
@@ -1,9 +1,30 @@
-class UncontrolledFormatString {
+class Test {
+    /* goodG2B1() - use goodsource and badsink by changing first true to false */
+    private void goodG2B1() throws Throwable
+    {
+        String data;
+        if (false)
+        {
+            /* INCIDENTAL: CWE 561 Dead Code, the code below will never run
+             * but ensure data is inititialized before the Sink to avoid compiler errors */
+            data = null;
+        }
+        else
+        {
 
-    void uncontrolledFormatString() {
-        String data = "foo";
-        System.out.format(data);
-        System.out.format("%s%n", data);
+            /* FIX: Use a hardcoded string */
+            data = "foo";
+            data -= ("bar"); // doesnt work because this is an expression then a literal
 
+        }
+
+        if (true)
+        {
+            if (data != null)
+            {
+                /* POTENTIAL FLAW: uncontrolled string formatting */
+                System.out.format(data);
+            }
+        }
     }
 }
--- pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/cwe/UncontrolledFormatStringRule.java
+++ pmd-java/src/main/java/net/sourceforge/pmd/lang/java/rule/cwe/UncontrolledFormatStringRule.java
@@ -9,9 +9,13 @@ import java.util.List;
 import org.jaxen.JaxenException;
 
 import net.sourceforge.pmd.lang.ast.Node;
+import net.sourceforge.pmd.lang.java.ast.ASTAssignmentOperator;
+import net.sourceforge.pmd.lang.java.ast.ASTBlock;
 import net.sourceforge.pmd.lang.java.ast.ASTLiteral;
+import net.sourceforge.pmd.lang.java.ast.ASTLocalVariableDeclaration;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryExpression;
 import net.sourceforge.pmd.lang.java.ast.ASTPrimaryPrefix;
+import net.sourceforge.pmd.lang.java.ast.ASTVariableDeclaratorId;
 import net.sourceforge.pmd.lang.java.rule.AbstractJavaRule;
 
 
@@ -20,16 +24,23 @@ public class UncontrolledFormatStringRule extends AbstractJavaRule {
     @Override
     public Object visit(ASTPrimaryExpression node, Object data) {
         if (isStringFormatExpression(node)) {
-            System.out.println("String literal found");
             try {
                 List<Node> children = node.findChildNodesWithXPath(
                         "./PrimarySuffix/Arguments/ArgumentList/Expression/PrimaryExpression/PrimaryPrefix");
                 if (!children.isEmpty()) {
-                    System.out.println("Found Arguments");
                     if (children.get(0).jjtGetNumChildren() != 0) {
-                        if (!(children.get(0).jjtGetChild(0) instanceof ASTLiteral)) {
-                            System.out.println("First child is not a literal");
-                            addViolation(data, node);
+                        Node potentialBadSink = children.get(0).jjtGetChild(0);
+                        if (!(potentialBadSink instanceof ASTLiteral)) {
+                            System.out.println("Potentially bad sink found");
+                            Node topBlock = findTopBlock(potentialBadSink);
+                            String variableImage = potentialBadSink.getImage();
+
+                            if (isLocalVariable(topBlock, variableImage)) {
+                                System.out.println("Potentially bad sink is a local variable");
+                                if (!checkAssignments(topBlock, variableImage)) {
+                                    addViolation(data, node);
+                                }
+                            }
                         }
                     }
                 }
@@ -56,5 +67,57 @@ public class UncontrolledFormatStringRule extends AbstractJavaRule {
             }
         }
         return false;
+    }
+
+    private Node findTopBlock(Node node) {
+        Node blockParent = node.getFirstParentOfType(ASTBlock.class);
+
+        if (blockParent == null) {
+            return node;
+        }
+        return findTopBlock(blockParent);
+    }
+
+    private boolean isLocalVariable(Node parent, String variableImage) {
+        List<ASTLocalVariableDeclaration> localVariableDeclarations = parent.findDescendantsOfType(ASTLocalVariableDeclaration.class);
+
+        for (ASTLocalVariableDeclaration localVariableDeclaration: localVariableDeclarations) {
+            System.out.println("Local variable declaration found");
+            Node node = localVariableDeclaration.getFirstDescendantOfType(ASTVariableDeclaratorId.class);
+            if (node.hasImageEqualTo(variableImage)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean checkAssignments(Node parentNode, String variableImage) {
+        List<ASTAssignmentOperator> assignments = parentNode.findDescendantsOfType(ASTAssignmentOperator.class);
+
+        for (ASTAssignmentOperator assignment: assignments) {
+            System.out.println("Assignment operator found");
+            Node directParent = assignment.jjtGetParent();
+            Node varPrefix = directParent.jjtGetChild(0).jjtGetChild(0).jjtGetChild(0);
+            if (varPrefix.hasImageEqualTo(variableImage)) {
+                System.out.println("Assignment is assigning desired variable");
+                Node afterAssignment = directParent.jjtGetChild(2);
+                List<ASTPrimaryPrefix> prefixes = afterAssignment.findDescendantsOfType(ASTPrimaryPrefix.class);
+                boolean unsafe = false;
+                for (ASTPrimaryPrefix prefix: prefixes) {
+                    System.out.println("Found assignment prefix");
+                    if (!(prefix.jjtGetChild(0) instanceof ASTLiteral)) {
+                        unsafe = true; // Do something with unsafe assignments to further check safety
+                    }
+                }
+                if (unsafe) {
+                    // at this point, the var has been assigned to something besides a literal,
+                    // the actual assignment should be checked to see if  what it's being assigned to is safe
+                    // will use recursion
+                    // TODO checkAssignmentOfAssignments()
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
